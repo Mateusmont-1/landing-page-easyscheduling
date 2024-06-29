7090c99b250fb726b34af56367e5194046cfeef4
Mateusmont-1 <mateus.monteiro112@gmail.com>
2024-06-28 22:02:32 -0300

    Alteração

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import '@/app/globals.css'
 import { ThemeProvider } from '@/providers/theme-provider'
 import { Header } from '@/components/common/header'
 import { Footer } from '@/components/common/footer'
 import { Analytics } from '@vercel/analytics/react'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
