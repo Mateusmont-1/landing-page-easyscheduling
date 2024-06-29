d13946db0662fac0f363b75bd3334094353cdbe7
Mateusmont-1 <mateus.monteiro112@gmail.com>
2024-06-28 21:28:09 -0300

    Alteração

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -4,7 +4,7 @@ import Typography from '@components/ui/typography'
 
 export function Footer() {
   return (
-    <footer className="flex flex-col h-20 items-center justify-center w-full border-t">
+    <footer className="flex flex-col h-24 items-center justify-center w-full border-t">
       <div className="w-full max-w-[1280px] md:px-8 px-4 flex place-content-center">
         <div className="gap-x-11 md:flex flex-1 hidden">
           <Link
